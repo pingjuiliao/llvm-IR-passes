f425e174c0d43545f3fd231eacef9cf2328fbc1f
pingjuiliao <pingjuiliao@gmail.com>
2019-02-20 12:27:58 -0800

    small edit

--- libfuzzer/tutorial/main.c
+++ libfuzzer/tutorial/main.c
@@ -1,7 +1,20 @@
 #include<stdio.h>
 #include<stdlib.h>
 
+void vuln();
 int
 main(int argc, char** argv) {
-    
+    int n;
+    scanf("%d", &n);    
+    if ( n == 1337 ) {
+        vuln();
+    }
+}
+
+void
+vuln(void) {
+    char buf[50] ;
+    read(0, buf, 0x100);
+    puts(buf);
+    return ;
 }
