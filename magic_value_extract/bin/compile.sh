605509311e79144e7c7eb118972bd0453377634a
pingjui <pingjuiliao@gmail.com>
2019-11-09 15:04:03 -0800

    bin->build

--- magic_value_extract/bin/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ $1 == "" ]] ; then
-	clang -Xclang -load -Xclang ./libLLVMmagic_value_extract.so main.c
-else
-	clang -Xclang -load -Xclang ./libLLVMmagic_value_extract.so $1
-fi
--- stackguard/bin/compile.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ $1 == "" ]] ; then
-	clang -Xclang -load -Xclang ./libLLVMstackguard.so main.c
-else
-	clang -Xclang -load -Xclang ./libLLVMstackguard.so $1
-fi
